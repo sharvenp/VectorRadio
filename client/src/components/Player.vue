97c1a6cf28af2c7a177e9f405ba811868bcf6989
Sharven Prasad Dhanasekar <sharven.dhanasekar@mail.utoronto.ca>
2022-03-06 21:25:39 -0500

    start writing css for client

--- client/src/components/Player.vue
+++ client/src/components/Player.vue
@@ -1,14 +1,42 @@
 <template>
   <div>
-    <h1 v-if="connection_status == 0">Loading...</h1>
-    <h1 v-else-if="connection_status == 2">Error</h1>
-    <div v-else>
-      <img :src="songMetadata['img']" width="300" />
-      <h1>{{ songMetadata["title"] }}</h1>
-      <h2>
-        <i>{{ songMetadata["artist"] }}</i>
-      </h2>
-      <button>Tune In!</button>
+    <div
+      class="d-flex flex-column vh-100 justify-content-center align-items-center"
+    >
+      <div v-if="connectionStatus === 0">
+        <div
+          class="spinner-border text-primary load-spinner"
+          role="status"
+        ></div>
+      </div>
+      <div v-else-if="connectionStatus === 1">
+        <button type="button" class="btn btn-primary tune-in" @click="tuneIn">
+          Tune In
+        </button>
+      </div>
+      <div v-else-if="connectionStatus === 2" class="w-100">
+        <img class="album-art" :src="songMetadata['img']" width="300" />
+        <h1 class="title mt-5 text-truncate">{{ songMetadata["title"] }}</h1>
+        <h3 class="artist text-truncate">
+          {{ songMetadata["artist"] }}
+        </h3>
+        <input
+          type="range"
+          class="form-range volume-slider mt-3"
+          v-model="volume"
+          min="1"
+          max="1000"
+          step="1"
+          @change="onVolumeChange"
+        />
+      </div>
+      <div v-else-if="connectionStatus === -1">
+        <h1>Could not connect to <b>VectorRadio</b> server</h1>
+        <p>
+          Please make sure the server is set up properly and refresh the page to
+          try again.
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -18,26 +46,32 @@ export default {
   name: "Player",
   data() {
     return {
-      connection_status: 0,
+      connectionStatus: 0,
       websocket: undefined,
       songMetadata: {},
       audioBuffers: [],
       audioContext: undefined,
+      gainNode: undefined,
       isPlaying: false,
+      isTunedIn: false,
+      volume: 1000,
     };
   },
   mounted() {
     this.audioContext = new (window.AudioContext ||
       window.webkitAudioContext)();
+    this.gainNode = this.audioContext.createGain();
+    this.gainNode.gain.value = this.volume / 1000;
+    this.gainNode.connect(this.audioContext.destination);
 
     this.websocket = new WebSocket(process.env.VUE_APP_SERVER_ADDRESS);
 
     this.websocket.onopen = () => {
-      this.connection_status = 1;
+      this.connectionStatus = 1;
     };
 
     this.websocket.onerror = () => {
-      this.connection_status = 2;
+      this.connectionStatus = -1;
     };
 
     this.websocket.onmessage = (event) => {
@@ -52,6 +86,9 @@ export default {
     };
   },
   methods: {
+    tuneIn() {
+      this.connectionStatus = 2;
+    },
     updateSongMetadata(songMetadata) {
       this.songMetadata = songMetadata;
     },
@@ -70,7 +107,7 @@ export default {
 
       let source = this.audioContext.createBufferSource();
       source.buffer = nextBuffer;
-      source.connect(this.audioContext.destination);
+      source.connect(this.gainNode);
       source.onended = this.play;
       source.start();
     },
@@ -97,8 +134,64 @@ export default {
         this.play();
       }
     },
+    onVolumeChange() {
+      this.gainNode.gain.setValueAtTime(
+        this.volume / 1000,
+        this.audioContext.currentTime
+      );
+    },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.load-spinner {
+  width: 25vh;
+  height: 25vh;
+}
+
+.tune-in {
+  width: 20vh;
+  height: 10vh;
+  font-size: 1.5rem;
+  border-radius: 25px !important;
+}
+
+.album-art {
+  border-radius: 15px;
+  border: 2px black solid;
+  width: 70%;
+}
+
+.title {
+  font-size: 30px;
+  font-weight: bold;
+}
+
+.artist {
+  font-size: 15px;
+  font-style: italic;
+}
+
+.volume-slider {
+  width: 50%;
+}
+
+@media screen and (min-width: 1000px) {
+  .album-art {
+    width: 20%;
+  }
+  .title {
+    font-size: 50px;
+  }
+  .artist {
+    font-size: 20px !important;
+  }
+  .tune-in {
+    font-size: 3rem;
+  }
+  .volume-slider {
+    width: 10%;
+  }
+}
+</style>
